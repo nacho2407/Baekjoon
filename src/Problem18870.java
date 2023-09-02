07e2a925878db8b5554dcfdca5a3e3a8eabcfb92
Minjun <jmj0ok@kumoh.ac.kr>
2023-09-02 17:39:15 +0900

    9월 2일 풀이

--- src/Problem18870.java
+++ src/Problem18870.java
@@ -2,71 +2,61 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Problem18870 {
-
-    /*
-     * 풀이중
-     */
-    
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int pos[] = new int[Integer.parseInt(br.readLine())];
-        int sorted[] = new int[pos.length];
+        Node values[] = new Node[Integer.parseInt(br.readLine())];
+        Node sorted[] = new Node[values.length];
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         br.close();
 
-        for(int i = 0; i < pos.length; i++) {
-            pos[i] = Integer.parseInt(st.nextToken());
-            sorted[i] = pos[i];
+        for(int i = 0; i < values.length; i++) {
+            Node newNode = new Node(Integer.parseInt(st.nextToken()));
+
+            values[i]  = newNode;
+            sorted[i] = newNode;
         }
 
-        java.util.Arrays.sort(sorted);
+        Arrays.sort(sorted, new Comparator<Node>() {
+            public int compare(Node n, Node m) {
+                return n.value - m.value;
+            }
+        });
+
+        int nextRank = 0;
+
+        sorted[0].rank = nextRank;
+
+        for(int i = 1; i < sorted.length; i++) {
+            if(sorted[i-1].value != sorted[i].value) {
+                nextRank++;
+            }
+            
+            sorted[i].rank = nextRank;
+        }
 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int correction; // This variable is intended for duplicate value correction;
+        bw.write(values[0].rank + "");
 
-        if(pos[0] == sorted[0]) {
-            bw.write(0 + "");
-        }
-        else {
-            correction = 0;
-
-            for(int i = 1; i < sorted.length; i++) {
-                if(sorted[i] == sorted[i - 1]) {
-                    correction++;
-                    continue;
-                }
-
-                if(pos[0] == sorted[i]) {
-                    bw.write((i - correction) + "");
-                }
-            }
-        }
-
-        for(int i = 1; i < pos.length; i++) {
-            if(pos[i] == sorted[0]) {
-                bw.write(" " + 0);
-            }
-            else {
-                correction = 0;
-
-                for(int j = 1; j < sorted.length; j++) {
-                    if(sorted[j] == sorted[j - 1]) {
-                        correction++;
-                        continue;
-                    }
-
-                    if(pos[i] == sorted[j]) {
-                        bw.write(" " + (j - correction));
-                    }
-                }
-            }
+        for(int i = 1; i < values.length; i++) {
+            bw.write(" " + values[i].rank);
         }
 
         bw.close();
+    }
+}
+
+class Node {
+    int value;
+    int rank;
+
+    public Node(int newValue) {
+        value = newValue;
     }
 }
