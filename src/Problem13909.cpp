40bc14a036bf77e55f4a7ef5dd669ae0ec1f12f9
Minjun <jmj0ok@kumoh.ac.kr>
2024-04-09 02:54:10 +0900

    2024년 4월 8일 풀이

--- src/Problem13909.cpp
+++ src/Problem13909.cpp
@@ -2,7 +2,17 @@
 
 int main(void)
 {
-    // 풀이중
+    int n = 0;
+    std::cin >> n;
+
+    // 1과 자기 자신을 제외한 약수를 홀수개로 가지는 경우는 제곱수인 경우 밖에 없음!
+    int count = 0;
+    for(int i = 1; i * i <= n; i++)
+    {
+        count++;
+    }
+
+    std::cout << count << std::endl;
 
     return 0;
 }
