d26ef34dc6fca37dd31d652929a4e43f91a8e92d
Minjun <jmj0ok@kumoh.ac.kr>
2024-05-19 00:19:08 +0900

    2024년 5월 19일 풀이

---
+++ src/Problem9663.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+bool is_promising(int x, int y, std::vector<std::vector<int>> board);
+int n_Queen(int n, int level, std::vector<std::vector<int>> board);
+
+int main(void)
+{
+    int n = 0;
+    std::cin >> n;
+
+    std::vector<std::vector<int>> board;
+    for(int i = 0; i < n; i++)
+        board[i] = std::vector<int>();
+
+    // 작성중
+
+    return 0;
+}
